970dc5f1be49ab08fd90eeb1c313d0b3461471e8
walidMedhatOia <walidmedhat.oia@gmail.com>
2026-06-08 11:25:10 +0300

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <title>Alt CRM</title>
+        
+        <link rel="icon" type="image/x-icon" href="/assets/images/Fav icon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
