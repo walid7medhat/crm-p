842b7ad8a15271a8ea87b88afc8b7630f589b80b
walidMedhatOia <walidmedhat.oia@gmail.com>
2026-06-11 16:25:01 +0300

    user details

--- resources/js/pages/users/UserDetail.vue
+++ resources/js/pages/users/UserDetail.vue
@@ -79,7 +79,7 @@
                                         </div>
                                         <div class="col-12" v-if="user.last_login_location" >
                                                 <label class="col-3">Location:</label>
-                                                <span class=" text-muted col-6 mb-0">
+                                                <span class=" text-muted col-12 mb-0">
                                                     <i class="ri-map-pin-line me-1"></i>{{ user.last_login_location }}
                                                     <a v-if="user.last_login_lat && user.last_login_lng"
                                                        :href="mapsLink(user)"
